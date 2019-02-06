750b34fa26e5d4206b34f654921b627acb8664c8
Romain-David <romaindavid.sergeant@gmail.com>
2019-02-06 13:21:49 +0100

    Create Project JeuxDeSociete

---
+++ src/enedis/romaindavid/com/mastermind/Main.java
@@ -0,0 +1,7 @@
+package enedis.romaindavid.com.mastermind;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/enedis/romaindavid/com/rechercheplusmoins/Main.java
@@ -0,0 +1,7 @@
+package enedis.romaindavid.com.rechercheplusmoins;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
