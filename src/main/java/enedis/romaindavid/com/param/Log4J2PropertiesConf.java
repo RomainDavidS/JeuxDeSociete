c74cc28e4e60e5ae6ccf8d7f78c7537c8c8b178e
Romain-David <romaindavid.sergeant@gmail.com>
2019-02-20 16:54:17 +0100

    adding log4j library

--- src/main/java/enedis/romaindavid/com/param/Log4J2PropertiesConf.java
+++ src/main/java/enedis/romaindavid/com/param/Log4J2PropertiesConf.java
@@ -7,10 +7,6 @@ public class Log4J2PropertiesConf {
 
     private static Logger logger = LogManager.getLogger();
     public void performSomeTask(){
-        logger.debug("This is a debug message");
-        logger.info("This is an info message");
-        logger.warn("This is a warn message");
-        logger.error("This is an error message");
-        logger.fatal("This is a fatal message");
+
     }
 }
--- src/main/java/enedis/romaindavid/com/param/Title.java
+++ src/main/java/enedis/romaindavid/com/param/Title.java
@@ -1,19 +1,33 @@
 package enedis.romaindavid.com.param;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import static enedis.romaindavid.com.game.RecherchePlusMoins.*;
 import static enedis.romaindavid.com.game.Mastermind.*;
 
 import static enedis.romaindavid.com.param.Parameter.getNumberCasePossible;
 import static enedis.romaindavid.com.param.Parameter.getNumberTrialPossible;
 
+/**
+ * LOGS
+ *         logger.debug("This is a debug message");
+ *         logger.info("This is an info message");
+ *         logger.warn("This is a warn message");
+ *         logger.error("This is an error message");
+ *         logger.fatal("This is a fatal message");
+ */
 public abstract class Title {
+    private static Logger logger = LogManager.getLogger();
 
     public static void postTitlePlayerFirst(){
-        System.out.println("C'est l'ordinateur qui joue en premier");
+        logger.info("C'est l'ordinateur qui joue en premier");
     }
+
     public static void postTitlePcFirst(){
-        System.out.println("C'est le joueur qui joue en premier");
+        logger.info("C'est le joueur qui joue en premier");
     }
+
     public static void postSecretNumberPc(){
         postSecretNumber( "PC", getSecretNumberPC() );
     }
@@ -23,7 +37,7 @@ public abstract class Title {
     }
 
     private static void postSecretNumber(String player,String secretNumber){
-        System.out.println("(Combinaison secrète "+ player + " : "+  secretNumber + ")" );
+        logger.debug("(Combinaison secrète "+ player + " : "+  secretNumber + ")" );
     }
 
     public static void postTitleGameChallenger(){
@@ -51,11 +65,11 @@ public abstract class Title {
     }
 
     private static void postTitleGame(String name, String mode ){
-        System.out.println("*** Jeu du "+ getGameName() + " en mode "+  mode + ". Combinaison de "+ getNumberCasePossible() +" chiffres maximun et " + getNumberTrialPossible() + " essais possibles ***");
+        logger.info("*** Jeu du "+ getGameName() + " en mode "+  mode + ". Combinaison de "+ getNumberCasePossible() +" chiffres maximun et " + getNumberTrialPossible() + " essais possibles ***");
     }
 
     public static void postTitleTrial(){
-        System.out.println("Joueur : Essai n°" + getTrial() + " sur "+ getNumberTrialPossible() +". Veuillez saisir une combinaison.");
+        logger.info("Joueur : Essai n°" + getTrial() + " sur "+ getNumberTrialPossible() +". Veuillez saisir une combinaison.");
     }
 
     public static void postResultPlayer(){
@@ -67,7 +81,7 @@ public abstract class Title {
     }
 
     private static void postResult(String player,int numberTrial, String combinaison,String postResult ){
-        System.out.println(player + " : Proposition n° " + numberTrial + " sur "+ getNumberTrialPossible() + " : " + combinaison + " -> Réponse : " + postResult );
+        logger.info(player + " : Proposition n° " + numberTrial + " sur "+ getNumberTrialPossible() + " : " + combinaison + " -> Réponse : " + postResult );
     }
 
     public static void postTitleControllerSeizureLength(){
@@ -75,15 +89,15 @@ public abstract class Title {
     }
 
     private static void postTitleControllerSeizureLength(int len){
-        System.out.println("La longueur de la combinaison doit avoir une longueur max de "+ len + " chiffres maximum.");
+        logger.error("La longueur de la combinaison doit avoir une longueur max de "+ len + " chiffres maximum.");
     }
 
     public static void postTitleControllerFormat(){
-        System.out.println( "Erreur de format. Veuillez saisir une valeur numérique" );
+        logger.error( "Erreur de format. Veuillez saisir une valeur numérique" );
     }
 
     public static void postQuestionNumberSecret(){
-        System.out.println("Joueur : Veuillez saisir votre combinaison secrète");
+        logger.info("Joueur : Veuillez saisir votre combinaison secrète");
     }
 
     public static void postLostResultPC(){
@@ -95,7 +109,7 @@ public abstract class Title {
     }
 
     public static void postLostResult(String player, String secretNumber){
-        System.out.println( player + " : Désolé la combinaison secrète " + secretNumber + " n'a pas été trouvée." );
+        logger.info( player + " : Désolé la combinaison secrète " + secretNumber + " n'a pas été trouvée." );
     }
 
     public static void postWinResultPC(int postTrial){
@@ -111,14 +125,17 @@ public abstract class Title {
     }
 
     public static void postTitleMainMenu(){
-        System.out.println("A quel jeu souhaitez-vous jouer ? 1 -> Recherche + - : 2 -> Mastermind.");
+        logger.info("A quel jeu souhaitez-vous jouer ? 1 -> Recherche + - : 2 -> Mastermind.");
     }
+
     public static void postTitleControllerGameMenu(){
-        System.out.println("Votre choix doit égal à 1 ou 2.");
+        logger.error("Votre choix doit égal à 1 ou 2.");
     }
+
     public static void postTitleControllerModeMenu(){
-        System.out.println("Votre choix doit être égal à 1, 2 ou 3.");
+        logger.error("Votre choix doit être égal à 1, 2 ou 3.");
     }
+
     public static void postTitleModeMenuRechercherPlusMoins(){
         postTitleModeMenu("Recherche + -");
     }
@@ -126,7 +143,14 @@ public abstract class Title {
     public static void postTitleModeMenuMastermind(){
         postTitleModeMenu("Mastermind");
     }
+
     private static void postTitleModeMenu(String game){
-        System.out.println(game + " choisissez votre mode : 1 - Challenger : 2 - Défenseur : 3 - Duel");
+        logger.info(game + " choisissez votre mode : 1 - Challenger : 2 - Défenseur : 3 - Duel");
+
     }
+
+    public static void mainTitle(){
+        logger.info("Bonjour et bienvenue dans vos jeux de société.");
+    }
+
 }
