55ff5add87db6a47d489f4751fc78f1df52a89f4
Romain-David <romaindavid.sergeant@gmail.com>
2019-03-08 10:42:48 +0100

    add comments to interface Mode

--- src/main/java/enedis/romaindavid/com/game/Mode.java
+++ src/main/java/enedis/romaindavid/com/game/Mode.java
@@ -2,7 +2,21 @@ package enedis.romaindavid.com.game;
 
 public interface Mode {
 
+    /**
+     * for each game we must have a challenger mode
+     * @param game the chosen game either Mastermind or RecherchePlusMoins
+     */
     void challenger(Game game);
+
+    /**
+     * for each game we must have a defender mode
+     * @param game the chosen game either Mastermind or RecherchePlusMoins
+     */
     void defender(Game game);
+
+    /**
+     * for each game we must have a dual mode
+     * @param game the chosen game either Mastermind or RecherchePlusMoins
+     */
     void dual(Game game);
 }
